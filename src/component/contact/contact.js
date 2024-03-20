0431c9a91732436bd703a028f37ca17dd25b13c0
Vishu9354 <vishu0883386@gmail.com>
2024-03-20 22:55:32 +0530

    contatct page updated

--- src/component/contact/contact.js
+++ src/component/contact/contact.js
@@ -5,18 +5,16 @@ export default function contact() {
     <div className='main' style={{display:"flex",justifyContent:"space-around"}}>
       <div className='mail icon'>
       <i class="fa-solid fa-envelope"></i>
-      <p>Mail us</p>
-      <p> natureclub@iitjammu.ac.in</p>
+      <p><a href="natureclub@iitjammu.ac.in">Mail us</a></p>
+      {/* <p> natureclub@iitjammu.ac.in</p> */}
       </div>
       <div className='location icon'>
       <i class="fa-brands fa-linkedin"></i>
-      <p>linkedin</p>
-      <p> https://in.linkedin.com/in/nac-iit-jammu-551b3b27a</p>
+      <p><a href="https://in.linkedin.com/in/nac-iit-jammu-551b3b27a">Linked-in</a></p>
       </div>
       <div className='insta icon'>
       <i class="fa-brands fa-instagram"></i>
-      <p>Instagram</p>
-      <p> https://www.instagram.com/nac_iitjmu/</p>
+      <p><a href="https://www.instagram.com/nac_iitjmu/">Instagram</a></p>
       </div>
     </div>
   )
