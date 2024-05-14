c8213d8742ac7125557883b2ba6ea01ea5291b8c
Vishu9354 <vishu0883386@gmail.com>
2024-05-14 19:31:02 +0530

    inaugration page removed

--- src/component/home/home.js
+++ src/component/home/home.js
@@ -1,15 +1,15 @@
 import React from 'react'
 import Carousel from './carousel'
 import RecentEvent from './recent.jsx'
-import UpcomingEvent from './upcomingevent.js'
+// import UpcomingEvent from './upcomingevent.js'
 // import Inaugration from '../images/2.jpg'
-import Inaugration from './Inaugration.jsx'
+// import Inaugration from './Inaugration.jsx'
 import '../css/home.css'
 export default function home() {
 
   return (
     <>
-    <Inaugration/>
+    {/* <Inaugration/> */}
     <Carousel/>
    <RecentEvent/>
    {/* <UpcomingEvent/> */}
