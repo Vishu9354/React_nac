b8b0b26c53cbf0c73489f83009e9a7447d289404
Vishu9354 <vishu0883386@gmail.com>
2024-05-14 19:36:49 +0530

    navbar updated

--- src/component/navbar.js
+++ src/component/navbar.js
@@ -4,10 +4,10 @@ import './css/navbar.css'
 import {Link} from "react-router-dom"
 export default function navbar() {
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light" style={{position:"fixed",zIndex:"999991", width:"100%",}}>
+    <nav className="navbar navbar-expand-lg navbar-light bg-light" style={{zIndex:"999991", width:"100%",}}>
     <div className="container-fluid">
       <Link className="navbar-brand" to="/">
-      <img style={{width: "25vw",height:"15vh",marginLeft:"50px"}} src={logo} alt="anhad" />
+      <img src={logo} alt="anhad" />
       </Link>
       <button
         className="navbar-toggler"
